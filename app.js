65931f66d10053b606716d2ef110d8aedf8bb006
varaprasad-omniwyse <varaprasad.medarametla@omniwyse.com>
2020-05-10 12:12:38 +0530

    Added remoteok pull

--- app.js
+++ app.js
@@ -18,24 +18,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', async (req,res) => {
   try{
-    var now = moment(new Date());
     let response = [];
-    let feed = await parser.parseURL('https://weworkremotely.com/categories/remote-programming-jobs.rss');
-    feed.items.forEach(o => {
-      const pubDate = moment(o.pubDate);
-      if(now.diff(pubDate, 'hours') <= 24){//Published in last 24 hours
-        response.push({
-          title: o.title,
-          link: o.link,
-        });
-      }
-    });
+
+    let weWorkItems = await weWorkRemote();
+    response.push(...weWorkItems);
+
+    let remoteOkItems = await remoteOk();
+    response.push(...remoteOkItems);
+
     res.status(200).send(response);
   }catch(ex){
     console.log(ex);
     res.status(500).send(ex);
   }
 });
+
+async function remoteOk(){
+  let now = moment(new Date());
+  let feed = await parser.parseURL('https://remoteok.io/remote-jobs.rss');
+  return feed.items
+            .filter(o => now.diff(moment(o.pubDate), 'hours') <= 24)
+            .map(getCorrespondingItem);
+}
+
+async function weWorkRemote(){
+  let now = moment(new Date());
+  let feed = await parser.parseURL('https://weworkremotely.com/categories/remote-programming-jobs.rss');
+  return feed.items
+            .filter(o => now.diff(moment(o.pubDate), 'hours') <= 24)
+            .map(getCorrespondingItem);
+}
+
+function getCorrespondingItem(item){
+  return {
+    title: item.title.replace(/[\n\r\t]/g,''),
+    link: item.link,
+  }
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
